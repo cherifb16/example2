304e1bf8edc6407140e1c798a0849303c43ff04e
cherifb16 <cherifbobbyray@gmail.com>
2021-09-28 13:38:16 +0200

    sample codes

---
+++ test.rb
@@ -0,0 +1,18 @@
+require 'webrick'
+module WEBrick
+	  module HTTPServlet
+	    FileHandler.add_handler('rb', CGIHandler)
+	  end
+	end
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+	  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+	  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+	  Signal.trap(signal){ server.shutdown }
+	}
+server.mount('/test', WEBrick::HTTPServlet::FileHandler, 'test.html')
+# Add this line
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+server.start
